b5b9616da58fcf8d491cf0e2dca0bdef9e1b0063
StoneMortem <dluginbu@students.kennesaw.edu>
2026-01-31 00:32:28 -0500

    PigBasics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,44 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     private Vector2 moveInput;
-    public float speed = 5;
+    public float speed = 1.2f;
+    private Animator anim;
+    InputAction attackAction;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        attackAction = InputSystem.actions.FindAction("Attack");
     }
 
     void Update()
     {
+        anim.SetBool("RAttack", false);
         rb.linearVelocity = moveInput * speed;
+        if (rb.linearVelocity.x == 0 && rb.linearVelocity.y == 0)
+        {
+            anim.SetBool("isWalking", false);
+        }
+        
+        if (attackAction.IsPressed())
+        {
+            anim.Play("PigSwingRight");
+            
+            
+        }
+        
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        anim.SetBool("isWalking", true);
     }
+
+
+
+    
+
+
 }
