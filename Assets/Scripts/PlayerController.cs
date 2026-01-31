172587071a65869a71dac6e2382a27e7ceff210e
StoneMortem <dluginbu@students.kennesaw.edu>
2026-01-31 01:05:36 -0500

    End of "Friday"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,21 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject Attack;
+    BoxCollider2D AttackHB;
+    Transform tf;
     Rigidbody2D rb;
     private Vector2 moveInput;
     public float speed = 1.2f;
     private Animator anim;
     InputAction attackAction;
+    public bool facingRight = true;
+
 
     void Start()
     {
+        AttackHB = Attack.GetComponent<BoxCollider2D>();
+        tf = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         attackAction = InputSystem.actions.FindAction("Attack");
@@ -31,12 +38,33 @@ public class PlayerController : MonoBehaviour
             
             
         }
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("PigSwingRight"))
+        {
+            AttackHB.enabled = true;
+
+        }
+        else
+        {
+            AttackHB.enabled = false;
+        }
         
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        if (moveInput.x < 0)
+        {
+            facingRight = false;
+            tf.transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        if (moveInput.x > 0)
+        {
+            facingRight = true;
+            tf.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
         anim.SetBool("isWalking", true);
     }
 
