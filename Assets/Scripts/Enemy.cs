7327ed3db9164ef878e80ecfe195b930853300a6
Mikita Slabysh <slabysh2015@gmail.com>
2026-01-31 00:25:35 -0500

    Finished basics of melee enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
@@ -17,8 +19,34 @@ public class Enemy : MonoBehaviour
     public EnemyType enemyType;
     public State currentState;
 
-    public float health;
-    public float speed;
+    [SerializeField] protected float health;
+    [SerializeField] protected float wanderMoveSpeed;
+    [SerializeField] protected float attackMoveSpeed;
+    [SerializeField] protected float aggroRange;
+    [SerializeField] protected float attackDistance;
+    protected bool choosingDirection = false;
+    [SerializeField] protected float wanderWait = 2f;
 
-    public float aggroRange;
+    protected IEnumerator Wander()
+    {
+        // Choose a random direction to move in
+        Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized;
+        // Move in that direction for a short duration
+        float wanderDuration = Random.Range(5f, 10f);
+        float elapsed = 0f;
+        while (elapsed < wanderDuration)
+        {
+            transform.Translate(randomDirection * wanderMoveSpeed * Time.deltaTime, Space.World);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        StartCoroutine(Wait(wanderWait));
+    }
+
+    protected IEnumerator Wait(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        choosingDirection = false;
+    }
+    
 }
